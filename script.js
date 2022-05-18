a30a9c656a1c5a988371fb77cb76716dd2d39a27
Leanna Lucas <leannalucas10@gmail.com>
2022-05-18 16:32:31 +0100

    beautiful add to javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+let myName = prompt("What is your name?");
+
+console.log("Hello, "+ myName); 
+let pet = prompt("Do you prefer Cats or Dogs?");
+let title = "code:cookies"
+if (pet == "cat") {
+title = "meow:cookies";
+}
+else if (pet = "dog") {
+title = "woof:cookies";
+}
+
+document.write(title)
+
+
