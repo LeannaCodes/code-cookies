e5e6dd423bcc9d1914cfd310ba8b64e126e3a731
Leanna Lucas <leannalucas10@gmail.com>
2022-05-19 13:39:28 +0100

    add functions to code:cookies

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-let myName = prompt("What is your name?");
-
-console.log("Hello, "+ myName); 
-let pet = prompt("Do you prefer Cats or Dogs?");
-let title = "code:cookies"
-if (pet == "cat") {
-title = "meow:cookies";
+function getUsername() {
+    let myName = prompt("What is your name?");
+    return document.write(myName)
 }
-else if (pet = "dog") {
-title = "woof:cookies";
+function getPet() {
+    let pet = prompt("Do you prefer Cats or Dogs?");
+    let title = "code:cookies" 
+    if (pet == "cat") {
+        title = "meow:cookies";
+    }
+    else if (pet = "dog") {
+    title = "woof:cookies";
+    }
+    return document.write(title)
 }
 
-document.write(title)
-
+function getVegetable() {
+    let myVegetable = prompt("Name your favourite vegetable") 
+    return document.write(myVegetable)
+}
 
